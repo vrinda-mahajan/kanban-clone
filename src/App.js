4bd421807bbc09120683d3e923b8423ea117b7b9
vrinda-mahajan <vrindamahajan96@gmail.com>
2023-06-17 15:50:43 +0530

    styles: navbar done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
 import Home from "./pages/home/home";
+import "./App.css";
 
 function App() {
   return (
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,36 +8,39 @@ import {
   SearchNormal,
 } from "../../assets/icons/index";
 import { ProfileImg } from "../../assets/images";
+import "./navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar-logo-wrapper">
-        <h4>
+    <nav className="flex-r navbar">
+      <div className="flex-r navbar-logo-wrapper">
+        <div className="flex-r navbar-logo">
           <img src={Colorfilter} />
-          Project M.
-        </h4>
-        <img src={ArrowLeft} />
-      </div>
-      <div className="navbar-input-wrapper">
-        <img src={SearchNormal} />
-        <input placeholder="Search for anything..."></input>
-      </div>
-      <div>
-        <div className="navbar-icons-wrapper">
-          <img src={Calendar2}></img>
-          <img src={MessageQuestion}></img>
-          <img src={Notification}></img>
+          <h4>Project M.</h4>
         </div>
-        <div className="navbar-profile-wrapper">
-          <div className="navbar-profile-details">
-            <h6>Anima Agarwal</h6>
-            <p>U.P, India</p>
+        <img className="navbar-logo-icon" src={ArrowLeft} />
+      </div>
+      <div className="flex-r navbar-right-section">
+        <div className="navbar-input-wrapper">
+          <img alt="search-icon" src={SearchNormal} />
+          <input placeholder="Search for anything..." />
+        </div>
+        <div className="flex-r">
+          <div className="flex-r navbar-icons-wrapper">
+            <img src={Calendar2}></img>
+            <img src={MessageQuestion}></img>
+            <img src={Notification}></img>
           </div>
-          <div className="navbar-profile-img">
-            <img src={ProfileImg} />
+          <div className="flex-r navbar-profile-wrapper">
+            <div className="navbar-profile-details">
+              <h6>Anima Agarwal</h6>
+              <p>U.P, India</p>
+            </div>
+            <div className="navbar-profile-img">
+              <img src={ProfileImg} />
+            </div>
+            <img src={ArrowDown} />
           </div>
-          <img src={ArrowDown} />
         </div>
       </div>
     </nav>
