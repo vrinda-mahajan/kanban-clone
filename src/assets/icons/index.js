c6c3f7f644a83eb34129509ac268425af48f170b
vrinda-mahajan <vrindamahajan96@gmail.com>
2023-06-18 14:22:23 +0530

    added icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -9,6 +9,17 @@ import Comments from "./comments.svg";
 import ThreeDots from "./three-dots.svg";
 import Files from "./files.svg";
 import Plus from "./plus.svg";
+import AddSquare from "./add-square.svg";
+import Calendar from "./calendar.svg";
+import Category from "./category.svg";
+import Edit from "./edit.svg";
+import Filter from "./filter.svg";
+import Lamp from "./lamp-on.svg";
+import Link from "./link.svg";
+import Menu from "./menu.svg";
+import Pause from "./pause.svg";
+import Setting from "./setting.svg";
+import TaskSquare from "./task-square.svg";
 
 export {
   ArrowDown,
